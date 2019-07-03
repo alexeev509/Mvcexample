503c5e3a2941df4d836138f8a2a0896235875f60
Alekseev Yaroslav <alekseevy509@gmail.com>
2019-07-03 21:20:47 +0300

    little changes

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -9,16 +9,13 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 public class Main implements WebApplicationInitializer {
-    
+
 
     public void onStartup(ServletContext servletContext) throws ServletException {
         AnnotationConfigWebApplicationContext webApplicationContext=new AnnotationConfigWebApplicationContext();
-        webApplicationContext.register(ApplicationConfig.class);
+        // webApplicationContext.register(ApplicationConfig.class);
+        webApplicationContext.scan("server");
 //        System.out.println(webApplicationContext.getBeanDefinitionNames());
-
-        //servletContext.addListener(new ContextLoaderListener(webApplicationContext));
-
-      //  System.out.println("1111: "+webApplicationContext.getBean(ApplicationConfig.class));
 
         DispatcherServlet dispatcherServlet = new DispatcherServlet(webApplicationContext);
         ServletRegistration.Dynamic dynamic = servletContext.addServlet("dispatcher", dispatcherServlet);
