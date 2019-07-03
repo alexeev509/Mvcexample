a5dd5e5a315acaf5ca27ce3a4ba228ea4c996d7c
Alekseev Yaroslav <alekseevy509@gmail.com>
2019-07-03 21:15:28 +0300

    little changes

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -1,30 +1,15 @@
 package server;
 
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-import server.ApplicationConfig;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 public class Main implements WebApplicationInitializer {
-
-//    @Nullable
-//    protected WebApplicationContext createRootApplicationContext() {
-//        WebApplicationContext webApplicationContext=new AnnotationConfigWebApplicationContext();
-//        ServletContext servletContext = webApplicationContext.getServletContext();
-//
-//
-//        DispatcherServlet dispatcherServlet = new DispatcherServlet(webApplicationContext);
-//        ServletRegistration.Dynamic registration=servletContext.addServlet("dispatcher",dispatcherServlet);
-//        registration.addMapping("/*");
-//        registration.setLoadOnStartup(1);
-//
-//        return webApplicationContext;
-//    }
+    
 
     public void onStartup(ServletContext servletContext) throws ServletException {
         AnnotationConfigWebApplicationContext webApplicationContext=new AnnotationConfigWebApplicationContext();
